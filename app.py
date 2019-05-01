435810dda80dce7b3f5eeb5ba0a28a6156fa6593
IvanW <certified.courses@outlook.es>
2019-05-01 17:23:48 +0200

    endpont /store implementado

--- app.py
+++ app.py
@@ -1,36 +1,42 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
 stores = [
     {
-    'name':"Mi tienda increible",
-    'items':[
-        {
-            'name':'My item 1',
-            'price':15.99
-        }
-    ]
-}]
+        'name': "Mi tienda increible",
+        'items': [
+            {
+                'name': 'My item 1',
+                'price': 15.99
+            }
+        ]
+    }]
+
+
 # POST /store data: {name:}
-@app.route('/store',methods=['POST'])
+@app.route('/store', methods=['POST'])
 def create_store():
     pass
+
 
 # GET /store/<string:name>
 @app.route('/store/<string:name>')
 def get_store(name):
     pass
 
+
 # GET /store
 @app.route('/store')
 def get_stores():
-    pass
+    return jsonify({'stores':stores})
+
 
 # POST /store/<string:name>/item {name:,price:}
-@app.route('/store/<string:name>/item',methods=['POST'])
+@app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store():
     pass
+
 
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
